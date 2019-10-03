ac10e7ab597eaa7070b8e5d3b2ca020fd15f6376
nik-html <nik_html@mail.ru>
2019-10-03 15:47:00 +0500

    add folder first

---
+++ practic/main.js
@@ -0,0 +1,37 @@
+// /*Все параграфы внутри дивов */
+// $('div p')
+
+// /*выбираем элемент с id 'something' */
+// $('#something')
+
+// Выбираем элемент с классом 'something'
+// $('.something')
+
+// /*выбираем все ссылки из параграфа с id 'something' и с роддителем div*/
+// $('div p#something a')
+
+$(function(){
+
+	$('p').on('click', function(){
+		$(this).css({'font-weight':'bold'});
+	});
+
+	$('p').on('dblclick', function(){
+		$(this).fadeToggle('slow');
+	});
+
+	$('button#textNew').on('click', function(){
+		$('p#newText').text('Тебе ли ж бы что то изменить, теперь текст другой, доволен, пес?');
+	});
+
+	$('p').on('mouseenter', function(){
+		$(this).css({'font-style':'italic', 'text-align':'center'});
+	});
+
+	$('form input[type="submit"]').on('click', function(e){
+		e.preventDefault();
+		
+	});
+
+
+});
